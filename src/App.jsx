94b15d20e28b094476493588ca634927a380f491
tanishaagarwal12 <0708tanish@gmail.com>
2026-06-26 13:20:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  let name ="Tanisha";
+  return (
+    <>
+      <h1>Hii Everyone, I am {name}</h1>
+      <p> I am learning full stack development</p>
+    </>
+  )
+}
+
+
+export default App;
